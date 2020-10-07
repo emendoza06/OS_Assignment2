f4d5646e66fe259993a78dceca68f2f9f263e8f5
Epharra Mendoza <mendoza@hoare7.cs.umsl.edu>
2020-10-07 13:41:48 -0500

    Added signal handler to catch ctrl-c, still need children to terminate

--- master.c
+++ master.c
@@ -4,29 +4,44 @@
 #include <sys/ipc.h>
 #include <sys/stat.h>
 #include <time.h>
+#include <sys/wait.h>
+#include <sys/shm.h>
+#include <signal.h>
 
-#define SMSIZE 100
+/*------------------------------------------------GLOBALS AND PROTOTYPES-------------------------------*/
+/*-----------------------------------------------------------------------------------------------------*/
+
 #define PERMS (S_IRUSR | S_IWUSR)
 
 //Prototypes
 void read_the_clock();
+int free_memory();
+void sighandler(int);
 
 //Globals
-struct shared_memory_data{
-	int process_group_id;
-};
+typedef struct{
+	pid_t pgid;
+}shared_memory_data;
 
-struct shared_memory_data *shm_data;
+shared_memory_data *shm_data_ptr;
+
+//Memory globals
+shared_memory_data *shm_data_ptr;	//shared memory data pointer
+int shmid;				//shared memory segment id
 
 
-/*-----------------------------MAIN-----------------------------*/
-/*--------------------------------------------------------------*/
+
+
+
+/*------------------------------------------------------MAIN-----------------------------------------------*/
+/*----------------------------------------------------------------------------------------------------------*/
 int main(int argc, char* argv[]) {
-	key_t key; 
+	//library function to set a function to handle signal
+	signal(SIGINT, sighandler);
+	
+	//memory variables
+	int key; 
 	int shmid;
-	int shmdetach;
-	int shmcontrol; 
-	pid_t pid;
 
 
 	//Use ftok to convert pathname and id value 'a' to System V IPC key 
@@ -40,7 +55,7 @@ int main(int argc, char* argv[]) {
 	//Use shmget which allocates a shared memory segment and returns identifier of System V shared memory segmentassociated with the value of the argument key
 	//Store shared memory segment identifier into shmid 
 	//Sets permissions, creates a new segment, and fails if segment already exists
-	shmid = shmget(key, SMSIZE, PERMS | IPC_CREAT);  
+	shmid = shmget(key, sizeof(shared_memory_data), PERMS | IPC_CREAT);  
 	//If creation of shared memory segment fails then return with error message 
 	if(shmid == -1) { 
 		perror("\nERROR: Could not create shared memory segment"); 
@@ -48,8 +63,8 @@ int main(int argc, char* argv[]) {
 	}	
 
 	//Use shmat to attach shmid's shared memory segment to the address space of the calling process 
-	shm_data = (struct shared_memory_data*) shmat(shmid, NULL, 0); 
-	if (shm_data == (struct shared_memory_data*)(-1)) { 
+	shm_data_ptr = (shared_memory_data*) shmat(shmid, NULL, 0); 
+	if (shm_data_ptr == (void*)-1) { 
 		perror("\nERROR: Could not attach shared memory");
 		return 1;
 	}
@@ -103,25 +118,40 @@ int main(int argc, char* argv[]) {
 
 
 	//Create child process
+	pid_t pid;
 	pid = fork(); 
-	//If failed to create a new process then return with error message
-	if(pid == - 1) {
-		perror("\nERROR: Could not create child process"); 
-		return 1;
-	}
-	else if(pid == 0) {
+	
+	//If child was created
+	if(pid == 0) {
 		//Print processes id's
 		printf("\nChild process id is %u\n", getpid());
+		//The first fork sets the process_group_id where all other processes will be associated
+		shm_data_ptr->pgid = getpid();
+		printf("\n Stored process_group_id as :%d", shm_data_ptr->pgid);
 		printf("\nParent of child process id is %u\n", getppid());
 		printf("\nChild process will read the clock\n");
 		read_the_clock();
+		//sleep(1);
+		while(1);	//go into infinite loop to test parent termination
 	}
+	//If failed to create a new process then return with error message
+	else if(pid == - 1) {
+		perror("\nERROR: Could not create child process"); 
+	}
+	//Main (parent) process after fork succeeds
 	else { 
-		//A positive number is returned for pid of parent process
-		//Print processes id's
+		int returnStatus;
+		waitpid(pid, &returnStatus, 0);	//Parent process waits here for child to terminate
+
+		//Print process id's		
 		printf("\nParent of parent process is %u\n", getppid());
 		printf("\nParent process is %u\n", getpid());
+	
+		printf("\nParent will read the clock\n");
+		read_the_clock();	
 	}
+
+			
 
 	//Capture input file containing strings to be tested
 	char* fileName = argv[optind];
@@ -147,22 +177,7 @@ int main(int argc, char* argv[]) {
 
 	}
 	
-	//Detach from the memory segment 
-	shmdetach = shmdt(shm_data);
-	//If detachment fails then return with error message
-	if(shmdetach == -1) { 
-		perror("\nERROR: Could not detach memory segment"); 
-		return 1;
-	}	
-	//Remove memory segment 
-	shmcontrol = shmctl(shm_data);
-	//If removal of shared memory fails then return with error message 
-	if(shmcontrol == -1) { 
-		perror("\nERROR: Could not remove shared memory segment"); 
-		return 1;
-	}
-	
-	return 0;
+	return 0;	
 }
 
 //Algorithm for fetching current time is from techiedelight tutorial
@@ -181,14 +196,46 @@ void read_the_clock() {
 	struct tm *local = localtime(&now);
 
 	hours = local->tm_hour; 	//get hours since midnight(0-23)
-	minutes = local->tm_min; 	//get minutes passed after the hour (0-59)
+	minutes = local->tm_min;	//get minutes passed after the hour (0-59)
 	seconds = local->tm_sec;	//get seconds passed after minute (0-59)
 
 	//print local time 
-	if(hours < 12) {	//before midday 
-		printf("Time is : %02d:%02d:%02d am\n", hours, minutes, seconds);
+	if(hours < 12){		//before midday
+		printf("Time is : %02d:%02d:%02d am\n", hours, minutes, seconds); 
+	}
+	else if(hours == 12){
+		printf("Time is : %02d:%02d:%02d pm\n", hours, minutes, seconds);
 	}
 	else {			//after midday 
 		printf("Time is : %02d:%02d:%02d pm\n", hours -12, minutes, seconds);
 	}
+
 }
+
+int free_memory(){
+	//Detach from the memory segment
+	int shmdetach = shmdt(shm_data_ptr);
+	//If detachment fails then return with error message 
+	if(shmdetach == -1) { 
+		perror("\nERROR: Could not detach memory segment");
+		return 1;
+	}
+	//Remove memory segment 
+	int shmcontrol = shmctl(shmid, IPC_RMID, NULL);
+	//If removal of shared memory fails then return with error message
+	if(shmcontrol == -1) { 
+		perror("\nERROR: Could not remove shared memory segment");
+		return 1;
+	}
+}
+
+
+void sighandler(int signal){
+	killpg(shm_data_ptr->pgid, SIGTERM);
+	printf("\nTime children processes killed:");
+	read_the_clock();
+	
+//	while(wait(NULL) > 0);
+	
+//	free_memory();	
+}	
